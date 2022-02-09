66a86a050881145fb4151ae4a8e933f16934a2de
chuongduong <duongthuychuong@gmail.com>
2022-02-09 09:24:20 +0700

    add limit

--- backend/models/productDetail.js
+++ backend/models/productDetail.js
@@ -39,69 +39,6 @@ const productDetail = databaseServer.getDatabaseInstance().define('productDetail
         type: Sequelize.DATE
     }
 })
-// async function createPaymentLine(sapPurchase, eachBankAccount, CBA) {
-//   try {
-//     const PL = await paymentLine.create({
-//       sapPurchaseId: sapPurchase.sapPurchaseId,
-//       outGoingPaymentId: sapPurchase.outGoingPaymentId,
-//       bankAccountId: CBA.id,
-//       bankName: CBA.bankName,
-//       accountNumber: CBA.accountNumber,
-//       paidAmount: eachBankAccount.paidAmount,
-//       order: eachBankAccount.order,
-//     })
-//     return [null,PL]
-//   } catch (error) {
-//     return [error,null]
-//   }
-  
-// }
-// async function createPaymentLineNoBank(sapPurchase) {
-//   try {
-//     const PL = await paymentLine.create({
-//       sapPurchaseId: sapPurchase.sapPurchaseId,
-//       outGoingPaymentId: sapPurchase.outGoingPaymentId,
-//       bankAccountId: null,
-//       bankName: null,
-//       accountNumber: null,
-//       paidAmount: sapPurchase.totalAmount,
-//       order: 0,
-//     })
-//     return [null,PL]
-//   } catch (error) {
-//     return [error,null]
-//   }
-  
-// }
-
-// async function findAllPLByOGP (outGoingPaymentId) {
-//   try {
-//     const resultPaymentLine = await paymentLine.findAll({
-//       where: {
-//         outGoingPaymentId: outGoingPaymentId
-//       }
-//     })
-//     return [null,resultPaymentLine]
-//   } catch (error) {
-//     console.log('Error: ', error)
-//     return [error,null]
-//   }
-// }
-
-// async function listPaymentLineById (sapPurchaseId,outGoingPaymentId) {
-//   try {
-//     const resultPaymentLine = await paymentLine.findAll({
-//       where: {
-//         sapPurchaseId: sapPurchaseId,
-//         outGoingPaymentId: outGoingPaymentId
-//       }
-//     })
-//     return [null,resultPaymentLine]
-//   } catch (error) {
-//     console.log('Error: ', error)
-//     return [error,null]
-//   }
-// }
 module.exports = {
 
 }
