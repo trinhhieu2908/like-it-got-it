1b4c3f285629a3bf91e76ef4ecc72963a72eae7b
ORIENTSOFTWARE\hieu.trantrinh <hieu.trantrinh@orientsoftware>
2023-03-06 16:25:12 +0700

    no message

--- frontend/src/components/UI/LoadingSpinner.js
+++
@@ -1,7 +0,0 @@
-import styles from './LoadingSpinner.module.css';
-
-const LoadingSpinner = () => {
-  return <div className={styles.content}><div className={styles.spinner}></div></div>;
-}
-
-export default LoadingSpinner;
